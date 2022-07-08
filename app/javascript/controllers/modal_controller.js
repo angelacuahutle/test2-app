a8ef6e9c02144b51357d5a5d1cc0c67e53c51473
angelacuahutle <angela.cuahutle@gmail.com>
2022-07-08 16:24:02 -0500

    Provisional commit

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  connect() {
+
+  hideModal() {
+    this.modal.form.removeAttribute = new 
   }
 }
