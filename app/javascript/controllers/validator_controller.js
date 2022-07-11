bdab1744e15b98a3294ca433e8db2b6bdb4653f2
angelacuahutle <angela.cuahutle@gmail.com>
2022-07-11 10:48:04 -0500

    FInish validation controller but it does not work properly

--- app/javascript/controllers/validator_controller.js
+++ app/javascript/controllers/validator_controller.js
@@ -2,8 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ['submitbutton']
+  static targets = ['submitButton']
+  
   connect() {
     this.submitButtonTarget.hidden = true;
+  }
+
+  submit() {
+    console.log('checkpoint reached')
+    this.submitButtonTarget.click()
+    clearTimeout(this.timeout)
+    this.time = setTimeout(()=> {
+    this.submitButtonTarget.click()
+    }, 500)
   }
 }
