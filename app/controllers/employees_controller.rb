23fc7f4f9eeeb89d96c435e605c0e44b89d99daf
angelacuahutle <angela.cuahutle@gmail.com>
2022-07-13 10:57:13 -0500

    Employemnt modal works!

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-  #before_action :set_employee, only: %i[ show edit update destroy ]
+  # before_action :set_employee, only: %i[ show edit update destroy ]
 
   # GET /employees or /employees.json
   def index
@@ -8,8 +8,7 @@ class EmployeesController < ApplicationController
   end
 
   # GET /employees/1 or /employees/1.json
-  def show
-  end
+  def show; end
 
   # GET /employees/new
   def new
@@ -25,16 +24,15 @@ class EmployeesController < ApplicationController
   end
 
   # GET /employees/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /employees or /employees.json
   def create
     @employee = Employee.new(employee_params)
 
     respond_to do |format|
-      if @employee.save!
-        format.html { redirect_to root_path, notice: "Employee was successfully created." }
+      if @employee.save! && @employee.valid?
+        format.html { redirect_to root_path, notice: 'Employee was successfully created.' }
         format.json { render :show, status: :created, location: @employee }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,15 +41,11 @@ class EmployeesController < ApplicationController
     end
   end
 
-  def employment_data
-    @employee = Employee.new(employee_params)
-  end
-
   # PATCH/PUT /employees/1 or /employees/1.json
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to employee_url(@employee), notice: "Employee was successfully updated." }
+        format.html { redirect_to employee_url(@employee), notice: 'Employee was successfully updated.' }
         format.json { render :show, status: :ok, location: @employee }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -65,19 +59,21 @@ class EmployeesController < ApplicationController
     @employee.destroy
 
     respond_to do |format|
-      format.html { redirect_to employees_url, notice: "Employee was successfully destroyed." }
+      format.html { redirect_to employees_url, notice: 'Employee was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_employee
-      @employee = Employee.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def employee_params
-      params.require(:employee).permit(:first_name, :last_name, :email, :password, :phone_number, :date_employment_started, :employment, :date_employment_ended)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :password, :phone_number,
+                                     :date_employment_started, :employment, :date_employment_ended)
+  end
 end
